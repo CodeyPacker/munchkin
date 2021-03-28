eb5c872889611d7ecf7d9dc952c8e317fd2e5455
Cody Packer <codeypacker@gmail.com>
2021-03-28 02:09:59 -0600

    Got all equipment state working

--- components/CharacterScreen.js
+++ components/CharacterScreen.js
@@ -2,23 +2,23 @@ import _JSXStyle from "styled-jsx/style";
 import Image from "next/image";
 import Equipment from "./Equipment";
 
-const CharacterScreen = ({ playerLevel, handleLevelChange, name, gender, activeScreen, equipmentLevel }) => {
+const CharacterScreen = ({allEquipmentLevels, handleLevelChange, setAllEquipmentLevels, playerLevel, name, gender, activeScreen }) => {
   return (
     <div className={activeScreen !== "character-screen" ? "hide" : "show"}>
       <h2>{name} ({gender})</h2>
       <h2>Board Level</h2>
       <div className="character-level">
-        <button onClick={() => handleLevelChange("setPlayerLevel", -1)}>-1</button>
+        <button onClick={() => handleLevelChange(-1)}>-1</button>
         <span>{playerLevel}</span>
-        <button onClick={() => handleLevelChange("setPlayerLevel", +1)}>+1</button>
+        <button onClick={() => handleLevelChange(+1)}>+1</button>
       </div>
       <div className="character-power">
         <h2>Equipment Level - </h2>
-        <Equipment type="Helmet"/>
-        <Equipment type="Armor"/>
-        <Equipment type="Footgear"/>
-        <Equipment type="Weapons"/>
-        <Equipment type="Misc"/>
+        <Equipment allEquipmentLevels={allEquipmentLevels} setAllEquipmentLevels={setAllEquipmentLevels} type="helmet"/>
+        <Equipment allEquipmentLevels={allEquipmentLevels} setAllEquipmentLevels={setAllEquipmentLevels} type="armor"/>
+        <Equipment allEquipmentLevels={allEquipmentLevels} setAllEquipmentLevels={setAllEquipmentLevels} type="footgear"/>
+        <Equipment allEquipmentLevels={allEquipmentLevels} setAllEquipmentLevels={setAllEquipmentLevels} type="weapons"/>
+        <Equipment allEquipmentLevels={allEquipmentLevels} setAllEquipmentLevels={setAllEquipmentLevels} type="misc"/>
       </div>
       <Image
         className="player-icon"
